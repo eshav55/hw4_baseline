6681ebbd007a70cfc87089ac0dea0996b73d1976
eshav55 <evijay@umass.edu>
2023-12-05 15:52:30 -0500

    Add Javadoc comments to ExpenseTrackerModel class

--- expense_tracker/src/model/ExpenseTrackerModel.java
+++ expense_tracker/src/model/ExpenseTrackerModel.java
@@ -4,117 +4,131 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Represents the data model for an expense tracker application. This class manages
+ * a list of transactions and notifies observers when changes occur.
+ * It encapsulates the state of the application and enforces data integrity.
+ */
 public class ExpenseTrackerModel {
 
-    //encapsulation - data integrity
+    // List to store the transactions
     private List<Transaction> transactions;
+    // List to store indices of transactions that match a certain filter
     private List<Integer> matchedFilterIndices;
+    // List of observers to notify on model updates
+    private List<ExpenseTrackerModelListener> listeners;
 
-    // This is applying the Observer design pattern.                          
-    // Specifically, this is the Observable class. 
-
+    /**
+     * Constructs an ExpenseTrackerModel with no transactions and no observers.
+     */
     public ExpenseTrackerModel() {
-        transactions = new ArrayList<Transaction>();
-        matchedFilterIndices = new ArrayList<Integer>();
-    }
-
-    public void addTransaction(Transaction t) {
-        // Perform input validation to guarantee that all transactions added are non-null.
-        if (t == null) {
-            throw new IllegalArgumentException("The new transaction must be non-null.");
-        }
-        transactions.add(t);
-        // The previous filter is no longer valid.
-        matchedFilterIndices.clear();
-        stateChanged(); // Notify observers of the change
-    }
-
-    public void removeTransaction(Transaction t) {
-        transactions.remove(t);
-        // The previous filter is no longer valid.
-        matchedFilterIndices.clear();
-        stateChanged(); // Notify observers of the change
-    }
-
-    public List<Transaction> getTransactions() {
-        //encapsulation - data integrity
-        return Collections.unmodifiableList(new ArrayList<>(transactions));
-    }
-
-    public void setMatchedFilterIndices(List<Integer> newMatchedFilterIndices) {
-        // Perform input validation
-        if (newMatchedFilterIndices == null) {
-            throw new IllegalArgumentException("The matched filter indices list must be non-null.");
-        }
-        for (Integer matchedFilterIndex : newMatchedFilterIndices) {
-            if ((matchedFilterIndex < 0) || (matchedFilterIndex > this.transactions.size() - 1)) {
-                throw new IllegalArgumentException("Each matched filter index must be between 0 (inclusive) and the number of transactions (exclusive).");
-            }
-        }
-        // For encapsulation, copy in the input list 
-        this.matchedFilterIndices.clear();
-        this.matchedFilterIndices.addAll(newMatchedFilterIndices);
-        stateChanged();
-    }
-
-    public List<Integer> getMatchedFilterIndices() {
-        // For encapsulation, copy out the output list
-        List<Integer> copyOfMatchedFilterIndices = new ArrayList<Integer>();
-        copyOfMatchedFilterIndices.addAll(this.matchedFilterIndices);
-        return copyOfMatchedFilterIndices;
+        transactions = new ArrayList<>();
+        matchedFilterIndices = new ArrayList<>();
+        listeners = new ArrayList<>();
     }
 
     /**
-     * Registers the given ExpenseTrackerModelListener for
-     * state change events.
+     * Adds a transaction to the model and notifies all observers about the state change.
      *
-     * @param listener The ExpenseTrackerModelListener to be registered
-     * @return If the listener is non-null and not already registered,
-     *         returns true. If not, returns false.
+     * @param t the transaction to be added
+     * @throws IllegalArgumentException if the transaction is null
      */
-    private List<ExpenseTrackerModelListener> listeners = new ArrayList<>();
+    public void addTransaction(Transaction t) {
+        if (t == null) {
+            throw new IllegalArgumentException("Transaction cannot be null.");
+        }
+        transactions.add(t);
+        matchedFilterIndices.clear();
+        stateChanged();
+    }
 
+    /**
+     * Removes a transaction from the model and notifies all observers about the state change.
+     *
+     * @param t the transaction to be removed
+     */
+    public void removeTransaction(Transaction t) {
+        transactions.remove(t);
+        matchedFilterIndices.clear();
+        stateChanged();
+    }
+
+    /**
+     * Provides an unmodifiable view of the transactions list to prevent external modification.
+     *
+     * @return an unmodifiable list of transactions
+     */
+    public List<Transaction> getTransactions() {
+        return Collections.unmodifiableList(transactions);
+    }
+
+    /**
+     * Sets the indices of transactions that match a certain filter and notifies all observers
+     * about the state change.
+     *
+     * @param newMatchedFilterIndices a list of indices to be set
+     * @throws IllegalArgumentException if the list is null or indices are out of range
+     */
+    public void setMatchedFilterIndices(List<Integer> newMatchedFilterIndices) {
+        if (newMatchedFilterIndices == null) {
+            throw new IllegalArgumentException("Matched filter indices cannot be null.");
+        }
+        for (Integer index : newMatchedFilterIndices) {
+            if (index < 0 || index >= transactions.size()) {
+                throw new IllegalArgumentException("Invalid index: " + index);
+            }
+        }
+        matchedFilterIndices.clear();
+        matchedFilterIndices.addAll(newMatchedFilterIndices);
+        stateChanged();
+    }
+
+    /**
+     * Provides an unmodifiable view of the matched filter indices list to prevent external modification.
+     *
+     * @return an unmodifiable list of matched filter indices
+     */
+    public List<Integer> getMatchedFilterIndices() {
+        return Collections.unmodifiableList(matchedFilterIndices);
+    }
+
+    /**
+     * Registers an observer to be notified on model state changes.
+     *
+     * @param listener the observer to be registered
+     * @return true if registration is successful, false if the listener is null or already registered
+     */
     public boolean register(ExpenseTrackerModelListener listener) {
-        // For the Observable class, this is one of the methods.
-        //
-        // TODO
-
-        //return false;
         if (listener == null || listeners.contains(listener)) {
-            // Listener is null or already registered, so registration is not successful.
             return false;
         }
-        // Listener is non-null and not already registered, so add to the list.
         listeners.add(listener);
-        // Registration was successful.
         return true;
     }
 
+    /**
+     * Returns the number of registered observers.
+     *
+     * @return the number of observers
+     */
     public int numberOfListeners() {
-        // For testing, this is one of the methods.
-        //
-        //TODO
-        // Return the number of registered listeners
-        if (listeners.size()!=0)
-        {
-            return listeners.size();
-        }
-        return 0;
+        return listeners.size();
     }
 
+    /**
+     * Checks if a specific observer is registered.
+     *
+     * @param listener the observer to check
+     * @return true if the observer is registered, false otherwise
+     */
     public boolean containsListener(ExpenseTrackerModelListener listener) {
-        // For testing, this is one of the methods.
-        //
-        //TODO
         return listeners.contains(listener);
-        //return false;
     }
 
+    /**
+     * Notifies all registered observers about a state change in the model.
+     */
     protected void stateChanged() {
-        // For the Observable class, this is one of the methods.
-        //
-        //TODO
-        // Notify all registered listeners of the state change
         for (ExpenseTrackerModelListener listener : listeners) {
             listener.update(this);
         }
